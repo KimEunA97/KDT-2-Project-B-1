cb2a9c2c5512b4ef1a411ff27f633bba572ebf25
jungjieun <je_eun2735@naver.com>
2023-06-26 15:41:19 +0900

    CRA 말고 수동 설치 해보기 Fixes #8

---
+++ client/build/src/App.tsx
@@ -0,0 +1,12 @@
+import "./test.css"
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>랜딩페이지 만들기</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ dist/client/build/src/App.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./test.css");
+const react_1 = require("react");
+const App = () => {
+    return (react_1.default.createElement("div", null,
+        react_1.default.createElement("h1", null, "\uB79C\uB529\uD398\uC774\uC9C0 \uB9CC\uB4E4\uAE30")));
+};
+exports.default = App;
+//# sourceMappingURL=App.js.map
