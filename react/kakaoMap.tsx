e3a29476a1e5de5f77653769b79281c5883bdb20
jungjieun <je_eun2735@naver.com>
2023-06-27 15:39:46 +0900

    navigation 추가해봤지만 안됐음

--- react/kakaoMap.tsx
+++ react/kakaoMap.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { Map, MapMarker, Polyline } from 'react-kakao-maps-sdk';
 
 const KakaoMap = () => {
   const [state, setState] = useState({
@@ -53,17 +53,33 @@ const KakaoMap = () => {
         center={state.center}
         style={{
           width: '100%',
-          height: '450px',
+          height: '900px',
         }}
         level={3}
       >
         {!state.isLoading && (
           <MapMarker position={state.center}>
             <div style={{ padding: '5px', color: '#000' }}>
-              {state.errMsg ? state.errMsg : '여기에 계신가요?!'}
+              {state.errMsg ? state.errMsg : '현재위치'}
             </div>
           </MapMarker>
         )}
+        {/* 음식점 마커 */}
+        <MapMarker position={{ lat: 35.8716862, lng: 128.5646145 }}>
+          <div style={{ color: '#000' }}>대구 팔공막창</div>
+        </MapMarker>
+        <Polyline
+          path={[
+            [
+              { lat: 36.3493063, lng: 127.377581 },
+              { lat: 35.8716862, lng: 128.5646145 },
+            ],
+          ]}
+          strokeWeight={5} // 선의 두께 입니다
+          strokeColor={' blue '} // 선의 색깔입니다
+          strokeOpacity={0.7} // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+          strokeStyle={'solid'} // 선의 스타일입니다
+        />
       </Map>
     </>
   );
