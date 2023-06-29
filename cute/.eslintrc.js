44b0a99be49c683128d6a0b5361d085efc645eb9
jungjieun <je_eun2735@naver.com>
2023-06-29 11:15:12 +0900

    카카오 네비 띄우기 Fixes #28 시작

--- cute/.eslintrc.js
+++ cute/.eslintrc.js
@@ -1,14 +1,5 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    // rules에 다음과 같이 추가
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'LF',
-      },
-    ],
-  },
 };
+
--- cute/App.tsx
+++ cute/App.tsx
@@ -1,13 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
 import React from 'react';
-import {SafeAreaView} from 'react-native';
-import InputTest from './View/getExample';
+import KakaoMap from './View/KakaoMap';
 
 function App(): JSX.Element {
-  return (
-    <SafeAreaView>
-      <InputTest />
-    </SafeAreaView>
-  );
+  return <KakaoMap></KakaoMap>;
 }
 
 export default App;
