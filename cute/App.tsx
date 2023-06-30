958a8fa7d4e28970913a5a2837ff97e2b14af293
jungjieun <je_eun2735@naver.com>
2023-06-30 14:51:21 +0900

    현재위치 권한 설정

--- cute/App.tsx
+++ cute/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 // import InputTest from './View/getExample';
 import Map from './View/map';
+// import Test from './View/test';
 
 function App(): JSX.Element {
   return (
     <SafeAreaView>
       <Map />
+      {/* <Test></Test> */}
     </SafeAreaView>
   );
 }
