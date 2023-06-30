e80dead584add1957e9d83b0475392711c174dce
jungjieun <je_eun2735@naver.com>
2023-06-30 12:43:02 +0900

    출발-도착 마커

--- cute/View/map.tsx
+++ cute/View/map.tsx
@@ -1,6 +1,7 @@
 import MapView,{ Marker} from 'react-native-maps';
 import React from 'react';
 import { View } from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
 
 
 const App = () => {
@@ -15,9 +16,13 @@ const App = () => {
   <View>
   <MapView style={{ width: '100%', height: '100%' }} initialRegion={initialRegion} >
   <Marker
-          coordinate={{latitude: 37.541, longitude: 126.986}}
-          title="this is a marker"
-          description="this is a marker example"></Marker>
+          coordinate={{latitude: 36.3492357, longitude: 127.377491}}
+          title="출발"
+          description="그린컴퓨터아트학원"></Marker>
+  <Marker
+          coordinate={{latitude: 35.871686, longitude: 128.5646145}}
+          title="도착"
+          description="막창"></Marker>
   </MapView>
   </View>
   )
