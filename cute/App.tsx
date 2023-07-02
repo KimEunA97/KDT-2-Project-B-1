ee957ac6a755b3132e40c9b312eae67ed0571380
jungjieun <je_eun2735@naver.com>
2023-07-02 13:52:56 +0900

    maptest.tsx

--- cute/App.tsx
+++ cute/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 // import InputTest from './View/getExample';
 import Map from './View/map';
+// import Maptest from './View/maptest';
 
 function App(): JSX.Element {
   return (
     <SafeAreaView>
       <Map></Map>
+      {/* <Maptest></Maptest> */}
     </SafeAreaView>
   );
 }
