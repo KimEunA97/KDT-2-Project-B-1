92d3ee3b43b1bf08c24c2ec6696240c704acb307
HaekyungSeong <haekyungs95@gmail.com>
2023-07-04 22:02:22 +0900

    크롤링으로 맛집 이름 추출 성공~

--- cute/model/crawling.ts
+++ cute/model/crawling.ts
@@ -1,19 +1,26 @@
 import axios from 'axios';
-import RNFS, {DownloadDirectoryPath} from 'react-native-fs';
-// import {writeFile} from 'fs/promises';
+import cheerio, {CheerioAPI} from 'react-native-cheerio';
 
-export const axiosTest = async () => {
-  // 상대 경로로 하는 경우, 파일시스템에 직접적으로 액세스가 불가함으로,
-  // 앱 문서 디렉토리에 파일 저장을 위한 경로를 변수에 담는다.
-  const filePath = `${DownloadDirectoryPath}/test.txt`;
-  console.log(filePath);
-
+export const axiosTest = async (local: String, food: String) => {
   // fetch와 같은 HTTP요청 라이브러리
   try {
     const response = await axios.get(
-      'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EB%8C%80%EC%A0%84+%EB%91%90%EB%A3%A8%EC%B9%98%EA%B8%B0+%EB%A7%9B%EC%A7%91',
+      `https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=${encodeURIComponent(
+        `${local}+${food}+맛집`,
+      )}`,
     );
-    await RNFS.writeFile(`${filePath}`, `${JSON.stringify(response)}`, 'utf8');
+
+    const $ = cheerio.load(response.data);
+    const places: string[] = [];
+
+    $('span.place_bluelink.TYaxT').each(
+      (index: Number, element: CheerioAPI) => {
+        const placeName = $(element).text();
+        places.push(placeName);
+      },
+    );
+
+    console.log(places);
   } catch (err) {
     console.log(err);
   }
